b20ddab929bcebc57aea820492274f2c1d481eb8
Ahad Tariq <CaptainXAhad@outlook.com>
2021-11-29 07:07:47 +0500

    Working on data table

--- app/Views/attendance/data.php
+++ app/Views/attendance/data.php
@@ -1,1 +1,66 @@
-Attendance record
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title></title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+<nav class="navbar navbar-expand-sm bg-primary navbar-dark">
+    <a class="navbar-brand" href="index.php">Home</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="Profile">Profile</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="attendance">Attendance</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="auth/logout">Logout</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<div class="container" style="margin-top:30px">
+    <div class="card">
+        <div class="card-header">
+            <div class="row">
+                <div class="col-md-9">Attendance List</div>
+                <div class="col-md-3" align="right">
+                    <button type="button" id="report_button" class="btn btn-danger btn-sm">Report</button>
+                    <button type="button" id="add_button" class="btn btn-info btn-sm">Add</button>
+                </div>
+            </div>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <span id="message_operation"></span>
+                <table class="table table-striped table-bordered" id="attendance_table">
+                    <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Pak Number</th>
+                        <th>Rank</th>
+                        <th>Attendance Status</th>
+                        <th>Attendance Date</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
